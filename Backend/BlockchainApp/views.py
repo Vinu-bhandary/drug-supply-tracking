1244fecf258faf02ed5f5e10ab3ba143a7ae639f
ThrineshAV <avthrinesh@gmail.com>
2025-11-30 16:35:24 +0530

    implementing CURD operations without API End points

--- Backend/BlockchainApp/views.py
+++ Backend/BlockchainApp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.db import models
+from BlockchainApp.models import BlockchainTransaction
+from SupplyApp.models import Batch
 
-# Create your views here.
+# BlockchainTransaction CRUD
+def create_blockchain_tx(data):
+    return BlockchainTransaction.objects.create(**data)
+
+def read_blockchain_tx(tx_id):
+    return BlockchainTransaction.objects.get(id=tx_id)
+
+def update_blockchain_tx(tx_id, data):
+    obj = BlockchainTransaction.objects.get(id=tx_id)
+    for k, v in data.items():
+        setattr(obj, k, v)
+    obj.save()
+    return obj
+
+def delete_blockchain_tx(tx_id):
+    obj = BlockchainTransaction.objects.get(id=tx_id)
+    return obj.delete()
--- Backend/SupplyApp/views.py
+++ Backend/SupplyApp/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.db import models
+from SupplyApp.models import Batch, Order, OrderItem
+from MasterApp.models import Drug, Location, User
 
-# Create your views here.
+# Batch CRUD
+def create_batch(data):
+    return Batch.objects.create(**data)
+
+def read_batch(batch_id):
+    return Batch.objects.get(id=batch_id)
+
+def update_batch(batch_id, data):
+    obj = Batch.objects.get(id=batch_id)
+    for k, v in data.items():
+        setattr(obj, k, v)
+    obj.save()
+    return obj
+
+def delete_batch(batch_id):
+    obj = Batch.objects.get(id=batch_id)
+    return obj.delete()
+
+
+# Order CRUD
+def create_order(data):
+    return Order.objects.create(**data)
+
+def read_order(order_id):
+    return Order.objects.get(id=order_id)
+
+def update_order(order_id, data):
+    obj = Order.objects.get(id=order_id)
+    for k, v in data.items():
+        setattr(obj, k, v)
+    obj.save()
+    return obj
+
+def delete_order(order_id):
+    obj = Order.objects.get(id=order_id)
+    return obj.delete()
+
+
+# OrderItem CRUD
+def create_order_item(data):
+    return OrderItem.objects.create(**data)
+
+def read_order_item(item_id):
+    return OrderItem.objects.get(id=item_id)
+
+def update_order_item(item_id, data):
+    obj = OrderItem.objects.get(id=item_id)
+    for k, v in data.items():
+        setattr(obj, k, v)
+    obj.save()
+    return obj
+
+def delete_order_item(item_id):
+    obj = OrderItem.objects.get(id=item_id)
+    return obj.delete()
